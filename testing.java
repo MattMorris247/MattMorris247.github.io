e20f31afda583ee5c89c5dc42126caf906a18cf9
Matt Morris <73907438+MattMorris247@users.noreply.github.com>
2022-11-15 13:40:31 -0500

    Website Creation

---
+++ testing.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class testing extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("this is code from java", 10, 10);
+    }
+}
